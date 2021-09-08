9aaf5b7b2b87423e1e5848cd1459df19a96f188b
Skye M <skyemc608@gmail.com>
2021-09-07 22:58:19 -0500

    completed challenge 2

--- challenge2.js
+++ challenge2.js
@@ -6,3 +6,37 @@
 // - Withdraws 100.0 from Matt's account
 // - Deposits 100.0 to My account
 // - Prints both accounts
+
+class Bank {
+  constructor(name, balance) {
+    this.name = name;
+    this.balance = balance;
+  }
+  withdraw() {
+    let amount = prompt(
+      "Please enter the amount you would like to withdraw in the box below"
+    );
+    this.balance = this.balance - amount;
+    return this.balance;
+  }
+  deposit() {
+    let amount = prompt(
+      "Please enter the amount you would like to deposit in the box below"
+    );
+    this.balance = this.balance + amount;
+    return this.balance;
+  }
+  print() {
+    console.log(`Bank Account Name: ${this.name} Balance: ${this.balance} `);
+  }
+}
+
+let account1 = new Bank("Matt's Account", "1000");
+let account2 = new Bank("My Account", "0");
+
+account1.print();
+account2.print();
+account1.withdraw();
+account2.deposit();
+account1.print();
+account2.print();
